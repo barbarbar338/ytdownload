761b7d3b8174d6297cdd81a3cfb5f53a3d15c369
Barış DEMİRCİ <demirci.baris38@gmail.com>
2022-09-07 15:10:29 +0000

    🚀 CLI support

--- README.md
+++ README.md
@@ -1,17 +1,34 @@
 # Download YouTube Videos Easily
-<b>YTDownload</b> allows you to download youtube videos easily.
 
-## Usage
+<b>YTDownload</b> allows you to download youtube videos easily. Now with CLI support!
+
+## Usage [CLI]
+
+```bash
+$ ytdownload <video_url>
+```
+
+example:
+
+```bash
+$ ytdownload https://www.youtube.com/watch?v=dQw4w9WgXcQ
+```
+
+## Usage [Code]
+
 ```js
 /* ES6 */
-import ytdownload from "ytdownload";
+import { download } from "ytdownload";
 
 /* CommonJS*/
-const ytdownload = require("ytdownload");
+const { download } = require("ytdownload");
 
-ytdownload("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
-.then(VideoPath => {
-    console.log(VideoPath); // => ./videos/Rick Astley - Never Gonna Give You Up (Video).mp4
+download("video_url");
+
+// example
+download("https://www.youtube.com/watch?v=dQw4w9WgXcQ").then((VideoPath) => {
+	console.log(VideoPath); // => ./videos/Rick Astley - Never Gonna Give You Up (Video).mp4
 });
 ```
-[Contact Me For More Help](https://bariscodes.me/discord)
+
+[Contact Me For More Help](https://338.rocks/discord)
---
+++ src/download.ts
@@ -0,0 +1,69 @@
+import { mkdir, createWriteStream } from "fs";
+import { table } from "table";
+import ytdl, { getInfo } from "ytdl-core";
+import { clearLine, cursorTo } from "readline";
+
+export const downlaod = (url: string, logging = true): Promise<string> => {
+	return new Promise((resolve, reject) => {
+		mkdir("videos", (err) => {
+			if (err && err.code !== "EEXIST")
+				reject(
+					`[YTDownload] ${err} \n\nFOR MORE ADVANCED HELP: https://discord.com/invite/BjEJFwh`,
+				);
+			getInfo(url)
+				.then((info) => {
+					const video = ytdl(info.videoDetails.video_url);
+					const filePath = `./videos/${info.videoDetails.title}.mp4`;
+					video.pipe(createWriteStream(filePath));
+					if (logging) {
+						const data = table([
+							["YTDownload", "Status", "Percent"],
+							[
+								info.videoDetails.title,
+								"Starting Download",
+								"0%",
+							],
+						]);
+						console.info(data);
+					}
+					video.on("end", () => {
+						if (logging) {
+							const data = table([
+								["YTDownload", "Status", "Percent"],
+								[
+									info.videoDetails.title,
+									"Download Finished",
+									"100%",
+								],
+							]);
+							console.log(`\n${data}`);
+						}
+						resolve(filePath);
+					});
+					video.on("error", (error) =>
+						reject(
+							`[YTDownload] ${error} \n\nFOR MORE ADVANCED HELP: https://discord.com/invite/BjEJFwh`,
+						),
+					);
+					video.on("progress", (chunk, downloaded, total) => {
+						if (logging) {
+							const percent = (
+								(downloaded * 100) /
+								total
+							).toFixed(3);
+							clearLine(process.stdout, 0);
+							cursorTo(process.stdout, 0);
+							process.stdout.write(
+								`[YTDownload] Downloading ${percent}%`,
+							);
+						}
+					});
+				})
+				.catch((err) =>
+					reject(
+						`[YTDownload] ${err} \n\nFOR MORE ADVANCED HELP: https://discord.com/invite/BjEJFwh`,
+					),
+				);
+		});
+	});
+};
